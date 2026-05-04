3f240d14157f8ca5b65960ce38c75c3f4e655c67
AAhmeddRRagabb <ahmedragab000170@gmail.com>
2026-05-04 11:04:49 +0300

    built the first API

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # Main Workflow
 # ------------------------------------------------------
 
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/welcome")
+def welcome():
+    return {
+        'Hello' : 'Ahmed'
+    }
